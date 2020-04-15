76ded668617ddf6a71fa1386d1648fca6c4b77a0
Shane Quick <shanesfast@gmail.com>
2020-04-15 18:43:32 -0500

    Add Jason Web Tokens for Authentication

---
+++ middleware/auth.js
@@ -0,0 +1,22 @@
+const config = require('../config/keys');
+const jwt = require('jsonwebtoken');
+
+function auth(req, res, next) {
+  const token = req.header('x-auth-token');
+
+  // Check for token
+  if (!token) res.status(401).json({ msg: 'Not authorized.' });
+
+  try {
+    // Verify token
+    const decoded = jwt.verify(token, config.jwtSecret);
+    // Add user from payload
+    req.user = decoded;
+    next();
+  } catch(e) {
+    res.status(400).json({ msg: 'Token is not valid.' });
+  }
+
+}
+
+module.exports = auth;
--- server.js
+++ server.js
@@ -1,24 +1,29 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const items = require('./routes/api/items');
 const path = require('path');
+const config = require('./config/keys');
 
 const app = express();
 
-// BodyParser Middleware
-app.use(bodyParser.json());
+// BodyParser
+app.use(express.json());
 
 // DB Config
-const db = require('./config/keys').mongoURI;
+const db = config.mongoURI;
 
 // Connect to MongoDB
-mongoose.connect(db)
+mongoose.connect(db, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true
+})
   .then(console.log('MongoDB connected.'))
   .catch(err => console.log(err));
 
 // Use Routes
-app.use('/api/items', items);
+app.use('/api/items', require('./routes/api/items'));
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
 
 // Serve static assets if in production
 if (process.env.NODE_ENV === 'production') {
